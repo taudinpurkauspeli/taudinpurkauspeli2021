1e8b3a6663bd1b1914a0459d0316f778e295054a
Dennis <tmi.dennisapter@gmail.com>
2021-10-13 14:04:40 +0300

    Added status change menu
    Co-authored-by: amalia53 <amalia53@github.com>

--- taudinpurkauspeli/frontend/src/App.js
+++ taudinpurkauspeli/frontend/src/App.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import Navigationbar from './components/Navbar';
 
 const App = () => {
-  const user = true;
-  const admin = true;
+  const [user, setUser] = useState(false);
+  const [admin, setAdmin] = useState(false);
+  const [guest, setGuest] = useState(true);
+
+  const changeUser = () => {
+    setUser(true);
+    setGuest(false);
+    setAdmin(false);
+  };
+
+  const changeGuest = () => {
+    setUser(false);
+    setGuest(true);
+    setAdmin(false);
+  };
+
+  const changeAdmin = () => {
+    setUser(false);
+    setGuest(false);
+    setAdmin(true);
+  };
 
   return (
-    <Navigationbar user={user} admin={admin} />
+    <Navigationbar
+      user={user}
+      admin={admin}
+      guest={guest}
+      changeUser={changeUser}
+      changeAdmin={changeAdmin}
+      changeGuest={changeGuest}
+    />
   );
 };
 
