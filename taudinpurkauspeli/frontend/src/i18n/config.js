1e9c91bcac826686313f48d38553119a3001b93d
sovalke <sonja.valkeinen@helsinki.fi>
2021-10-26 15:24:12 +0300

    Browser remembers selected language when refreshed
    Co-authored-by: Henna Mäkiluoma <Miniaya@users.noreply.github.com>

--- taudinpurkauspeli/frontend/src/i18n/config.js
+++ taudinpurkauspeli/frontend/src/i18n/config.js
@@ -1,9 +1,14 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n.use(LanguageDetector).init();
 
 i18n.use(initReactI18next).init({
   fallbackLng: 'fi',
-  lng: 'fi',
+
+  // eslint-disable-next-line no-undef
+  lng: localStorage.getItem('i18nextLng') || 'fi',
   resources: {
     fi: {
       // eslint-disable-next-line global-require
