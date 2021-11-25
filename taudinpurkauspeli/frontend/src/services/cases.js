1a2984a1adfe847fbadc5a5c8656e4a4983f1d1b
sannahan <73829699+sannahan@users.noreply.github.com>
2021-11-25 14:26:59 +0200

    Test login headers
    Co-authored-by: Henna <Miniaya@users.noreply.github.com>

--- taudinpurkauspeli/frontend/src/services/cases.js
+++ taudinpurkauspeli/frontend/src/services/cases.js
@@ -5,7 +5,10 @@ const baseUrl = 'http://localhost:8080/api/cases';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
-  return request.then((response) => response.data);
+  return request.then((response) => {
+    console.log('frontend headers', response.headers);
+    return (response.data);
+  });
 };
 
 const create = (newObject) => {
