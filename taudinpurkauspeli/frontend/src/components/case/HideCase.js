24d648f4a684d2acd3d378196c3fc09e6836b1a6
Henna Mäkiluoma <henna.makiluoma@gmail.com>
2021-11-26 20:26:17 +0200

    Fixed update bug

--- taudinpurkauspeli/frontend/src/components/case/HideCase.js
+++ taudinpurkauspeli/frontend/src/components/case/HideCase.js
@@ -1,35 +1,19 @@
 /* eslint-disable linebreak-style */
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import service from '../../services/cases';
 
-const HideCase = ({ caseToBeHidden, hideCaseFunc }) => {
-  const c = caseToBeHidden;
-  let hideStatus = false;
-
-  /* istanbul ignore next */
-  if (hideCaseFunc == null) {
-    hideStatus = c.hidden;
-  }
-
-  const [newHidden, setNewHidden] = useState(hideStatus);
+const HideCase = ({ c, hideCase }) => {
   const { t } = useTranslation();
 
-  const hideCase = (event) => {
+  const [newHidden, setNewHidden] = useState(c.hidden);
+
+  const handleVisibilityUpdate = (event) => {
     event.preventDefault();
-    // eslint-disable-next-line no-param-reassign
-    const caseObject = ({
+    hideCase({
       title: c.title,
       anamnesis: c.anamnesis,
       hidden: newHidden,
     });
-
-    /* istanbul ignore else */
-    if (hideCaseFunc != null) {
-      hideCaseFunc(caseObject);
-    } else {
-      service.update(c.id, caseObject);
-    }
   };
 
   const handleHiddenChange = () => {
@@ -38,7 +22,7 @@ const HideCase = ({ caseToBeHidden, hideCaseFunc }) => {
 
   return (
     <div>
-      <form onSubmit={hideCase} className="buttonLeft">
+      <form onSubmit={handleVisibilityUpdate} className="buttonLeft">
         <label htmlFor="submit" className="hidden">{newHidden ? t('buttonShowCaseToStudents') : t('buttonHideCaseFromStudents')}</label>
         <input type="submit" onClick={handleHiddenChange} id="submit" value={newHidden ? t('buttonShowCaseToStudents') : t('buttonHideCaseFromStudents')} />
       </form>
