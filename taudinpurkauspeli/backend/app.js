30df52a62e8e843445b52cc224b6f4ae8423bcce
Henna Mäkiluoma <henna.makiluoma@gmail.com>
2021-12-01 16:11:35 +0200

    Trying to expose more headers

--- taudinpurkauspeli/backend/app.js
+++ taudinpurkauspeli/backend/app.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV !== 'test') {
   });
 }
 
-app.use(cors());
+app.use(cors({ exposedHeaders: 'hyPersonStudentId'}));
 app.use(express.json());
 app.use(express.static('build'));
 app.use(middleware.requestLogger);
