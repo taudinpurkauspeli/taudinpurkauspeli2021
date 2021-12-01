5832dd52fbca915d61fd30805e45c1e17d50987c
asahola <amalia.ahola@gmail.com>
2021-12-01 16:18:55 +0200

    Update anamnesis

---
+++ taudinpurkauspeli/frontend/src/components/anamnesis/UpdateAnamnesis.js
@@ -0,0 +1,66 @@
+/* eslint-disable linebreak-style */
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Form } from 'react-bootstrap';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+const UpdateAnamnesis = ({ c, updateAnamnesis }) => {
+  const { t } = useTranslation();
+
+  const [anamnesis, setAnamnesis] = useState(c.anamnesis);
+
+  const caseSchema = Yup.object().shape({
+    title: Yup.string()
+      .min(2, t('warningShort'))
+      .max(5000, t('warningLong'))
+      .required(t('warningRequired')),
+  });
+
+  const handleAnamnesisUpdate = (values) => {
+    updateAnamnesis({
+      title: c.title,
+      anamnesis: values.anamnesis,
+      hidden: c.hidden,
+    });
+
+    setAnamnesis(values.anamnesis);
+  };
+
+  return (
+    <Formik
+      initialValues={{
+        anamnesis,
+      }}
+      validationSchema={caseSchema}
+      onSubmit={handleAnamnesisUpdate}
+    >
+      {({
+        handleSubmit,
+        handleChange,
+        values,
+        errors,
+      }) => (
+        <Form noValidate onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="updateTitle" style={{ width: '30rem' }}>
+            <Form.Control
+              type="text"
+              name="title"
+              anamnesis={values.anamnesis}
+              onChange={handleChange}
+              isInvalid={!!errors.anamnesis}
+            />
+            <Form.Text className="text-muted">
+              {t('caseAnamnesisInstruction')}
+            </Form.Text>
+            <Form.Control.Feedback type="invalid" role="alert" aria-label="from feedback">
+              {errors.title}
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default UpdateAnamnesis;
