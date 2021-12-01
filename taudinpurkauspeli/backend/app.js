eaba760dc7588c85dab01d8734d8429e5ea29741
Henna Mäkiluoma <henna.makiluoma@gmail.com>
2021-12-01 15:46:18 +0200

    More testing with headers

--- taudinpurkauspeli/backend/app.js
+++ taudinpurkauspeli/backend/app.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV !== 'test') {
   });
 }
 
-app.use(cors({ exposedHeaders: 'cn'}));
+app.use(cors());
 app.use(express.json());
 app.use(express.static('build'));
 app.use(middleware.requestLogger);
