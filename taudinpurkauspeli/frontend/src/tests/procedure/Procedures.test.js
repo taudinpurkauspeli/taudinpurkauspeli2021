be427f9169a13c57daf7ae02aed3578930151b3f
sovalke <sonja.valkeinen@helsinki.fi>
2021-12-04 20:40:28 +0200

    Tests for Profile & Procedures

---
+++ taudinpurkauspeli/frontend/src/tests/procedure/Procedures.test.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import ShallowRenderer from 'react-shallow-renderer';
+import { waitFor } from '@testing-library/react';
+import Procedures from '../../components/procedure/Procedures';
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: () => ({
+    id: '1',
+  }),
+  useRouteMatch: () => ({ url: '/cases/id/procedures' }),
+}));
+
+test('List and accordion are rendered', async () => {
+  const resultA = new ShallowRenderer();
+  resultA.render(<Procedures />);
+  const result = resultA.getRenderOutput();
+  await waitFor(() => expect(result).toBeDefined());
+  await waitFor(() => expect(result.type).toBe('div'));
+});
