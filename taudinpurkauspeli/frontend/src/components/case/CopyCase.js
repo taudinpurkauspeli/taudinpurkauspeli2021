a120763dd95326b35786d358344e7dfc78be3e64
asahola <amalia.ahola@gmail.com>
2021-12-09 16:45:31 +0200

    worked on copying the diff groups when copying the case
    Co-authored-by: Henna <Miniaya@users.noreply.github.com>
    Co-authored-by: Sonja <sonja.valkeinen@helsinki.fi>

--- taudinpurkauspeli/frontend/src/components/case/CopyCase.js
+++ taudinpurkauspeli/frontend/src/components/case/CopyCase.js
@@ -1,19 +1,58 @@
 /* eslint-disable linebreak-style */
+
 /* eslint-disable react/react-in-jsx-scope */
 import { Button } from 'react-bootstrap';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import caseService from '../../services/cases';
-import ducService from '../../services/differentials/differentialsUnderCases';
 import pucService from '../../services/proceduresUnderCase';
 
+// Caseen liitetyt diffiryhmät
+import ducGroupService from '../../services/differentials/differentialGroupsUnderCases';
+
+// diffiryhmään liitetyt diffit
+import ducService from '../../services/differentials/differentialsUnderCases';
+
 const copyCase = ({
-  caseToBeCopied, createProcedures, createDifferentials,
+  caseToBeCopied, createProcedures,
 }) => {
   const { t } = useTranslation();
   const oldCaseId = caseToBeCopied.id;
   // eslint-disable-next-line no-unused-vars
   let newCopyId = 0;
+
+  /* istanbul ignore next */
+  const copyTheDamnThing = (newCaseId, result) => {
+    console.log(result);
+    let duckling = {};
+
+    if (result == null) {
+      console.log('Kääk!');
+    } else {
+      result.forEach((element) => {
+        console.log(element.id);
+        duckling = {
+          caseId: newCaseId,
+          differentialGroupId: element.id,
+        };
+        ducGroupService.create(duckling).then((res) => {
+          const diffGroupCaseId = res.id;
+          ducService.getAll(element.differentialGroupId)
+            .then((result2) => {
+              result2.forEach((element2) => {
+                const did = element2.differentialId;
+                const plaa = {
+                  diffGroupCaseId,
+                  differentialId: did,
+                  description: element2.description,
+                };
+                ducService.create(plaa);
+              });
+            });
+        });
+      });
+    }
+  };
 
   /* istanbul ignore next */
   const handleCopy = (event) => {
@@ -28,15 +67,15 @@ const copyCase = ({
         newCopyId = copy.id;
       })
       .then(() => {
-        ducService.getAll(oldCaseId)
-          .then((result) => {
-            createDifferentials(newCopyId, result);
+        ducGroupService.getAll(oldCaseId)
+          .then((res) => {
+            copyTheDamnThing(newCopyId, res);
           });
       })
       .then(() => {
         pucService.getAll(oldCaseId)
-          .then((result) => {
-            createProcedures(newCopyId, result);
+          .then((res) => {
+            createProcedures(newCopyId, res);
           });
       });
   };
