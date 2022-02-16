7ff0220e2d7123be7e38dabf4338ef64f7b2c74a
sannahan <73829699+sannahan@users.noreply.github.com>
2022-02-16 13:09:46 +0200

    Refactor form

--- taudinpurkauspeli/frontend/src/App/case/components/NewCaseForm.js
+++ taudinpurkauspeli/frontend/src/App/case/components/NewCaseForm.js
@@ -1,83 +1,66 @@
 /* eslint-disable linebreak-style */
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Formik } from 'formik';
+import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { Form, Button } from 'react-bootstrap';
 
 const newCaseForm = ({ addCase }) => {
   const { t } = useTranslation();
 
-  const newCaseSchema = Yup.object().shape({
-    title: Yup.string()
-      .min(2, t('warningShort'))
-      .max(999, t('warningLong'))
-      .required(t('warningRequired')),
-    anamnesis: Yup.string(),
-    hidden: Yup.bool(),
+  const formik = useFormik({
+    initialValues: {
+      title: '',
+      anamnesis: '',
+      hidden: false,
+    },
+    validationSchema: Yup.object({
+      title: Yup.string()
+        .min(2, t('warningShort'))
+        .max(999, t('warningLong'))
+        .required(t('warningRequired')),
+      anamnesis: Yup.string(),
+      hidden: Yup.bool(),
+    }),
+    onSubmit: (values) => {
+      addCase({
+        title: values.title,
+        anamnesis: values.anamnesis,
+        hidden: values.hidden,
+      });
+    },
   });
 
-  const handleCaseAdd = (values) => {
-    addCase({
-      title: values.title,
-      anamnesis: values.anamnesis,
-      hidden: values.hidden,
-    });
-  };
-
   return (
-    <Formik
-      initialValues={{
-        title: '',
-        anamnesis: '',
-        hidden: false,
-      }}
-      validationSchema={newCaseSchema}
-      onSubmit={handleCaseAdd}
-    >
-      {({
-        handleSubmit,
-        handleChange,
-        values,
-        errors,
-      }) => (
-        <Form noValidate onSubmit={handleSubmit} spellCheck="false">
-          <Form.Group md="6" controlId="title">
-            <Form.Label>{t('caseTitle')}</Form.Label>
-            <Form.Control
-              type="text"
-              name="title"
-              value={values.title}
-              onChange={handleChange}
-              isInvalid={!!errors.title}
-            />
-            <Form.Control.Feedback type="invalid">
-              {errors.title}
-            </Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="anamnesis">
-            <Form.Label>{t('caseAnamnesis')}</Form.Label>
-            <Form.Control
-              as="textarea"
-              name="anamnesis"
-              rows={3}
-              value={values.anamnesis}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="hidden">
-            <Form.Check
-              required
-              name="hidden"
-              label={t('hideCase')}
-              value={values.hidden}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Button className="submitButton" id="submit-case" type="submit">{t('buttonSubmitNewCase')}</Button>
-        </Form>
-      )}
-    </Formik>
+    <Form noValidate onSubmit={formik.handleSubmit} spellCheck="false">
+      <Form.Group md="6" controlId="title">
+        <Form.Label>{t('caseTitle')}</Form.Label>
+        <Form.Control
+          type="text"
+          {...formik.getFieldProps('title')}
+          isInvalid={!!formik.errors.title}
+        />
+        <Form.Control.Feedback type="invalid">
+          {formik.errors.title}
+        </Form.Control.Feedback>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="anamnesis">
+        <Form.Label>{t('caseAnamnesis')}</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={3}
+          {...formik.getFieldProps('anamnesis')}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="hidden">
+        <Form.Check
+          required
+          label={t('hideCase')}
+          {...formik.getFieldProps('hidden')}
+        />
+      </Form.Group>
+      <Button className="submitButton" id="submit-case" type="submit">{t('buttonSubmitNewCase')}</Button>
+    </Form>
   );
 };
 
