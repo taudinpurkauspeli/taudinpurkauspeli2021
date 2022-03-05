22ac65c5fe434fac3b959622ae2ba025d7e32ae6
Henna Mäkiluoma <henna.makiluoma@gmail.com>
2022-03-05 23:43:14 +0200

    refactored option forms to use formik hook

--- taudinpurkauspeli/frontend/src/App/subprocedure/components/interviewSubProcedure/NewOptionForm.js
+++ taudinpurkauspeli/frontend/src/App/subprocedure/components/interviewSubProcedure/NewOptionForm.js
@@ -4,99 +4,82 @@ import { useSelector } from 'react-redux';
 import {
   Form, Button,
 } from 'react-bootstrap';
-import { Formik } from 'formik';
+import { useFormik, FormikProvider } from 'formik';
 import * as Yup from 'yup';
 import CustomTypeaheadSelect from '../../../../utils/CustomTypeaheadSelect';
 
 const NewOptionForm = ({ addOption }) => {
   const { t } = useTranslation();
-
-  const AddCheckSchema = Yup.object().shape({
-    name: Yup.string()
-      .min(2, t('warningShort'))
-      .max(999, t('warningLong'))
-      .required(t('warningRequired')),
-    description: Yup.string(),
-    isRequired: Yup.number()
-      .required(t('warningRequired'))
-      .integer(),
-  });
-
   const options = useSelector((state) => state.options);
 
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      description: '',
+      isRequired: '',
+    },
+    validationSchema: Yup.object().shape({
+      name: Yup.string()
+        .min(2, t('warningShort'))
+        .max(999, t('warningLong'))
+        .required(t('warningRequired')),
+      description: Yup.string(),
+      isRequired: Yup.number()
+        .required(t('warningRequired'))
+        .integer(),
+    }),
+    onSubmit: async (values) => {
+      const option = options.filter((r) => r.name === values.name);
+      addOption(
+        option.length === 0
+          ? values
+          : { ...option[0], ...values },
+      );
+    },
+  });
+
   return (
-    <Formik
-      initialValues={{
-        name: '',
-        description: '',
-        isRequired: 2,
-      }}
-      validationSchema={AddCheckSchema}
-      onSubmit={async (values) => {
-        const option = options.filter((r) => r.name === values.name);
-        addOption(
-          option.length === 0
-            ? values
-            : { ...option[0], ...values },
-        );
-      }}
-    >
-      {({
-        handleSubmit,
-        handleChange,
-        setFieldValue,
-        values,
-      }) => (
-        <div>
-          <Form onSubmit={handleSubmit}>
-            <CustomTypeaheadSelect
-              name="name"
-              label={t('addOption')}
-              options={options}
-            />
-            <Form.Group controlId="description">
-              <Form.Label>{t('description')}</Form.Label>
-              <Form.Control
-                as="textarea"
-                rows={3}
-                value={values.description}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            <Form.Group controlId="isRequired">
-              <Form.Check
-                defaultChecked
-                label={t('required')}
-                name="isRequired"
-                type="radio"
-                id="inline-radio-1"
-                value={2}
-                onChange={() => setFieldValue('isRequired', 2)}
-              />
-              <Form.Check
-                label={t('voluntary')}
-                name="isRequired"
-                type="radio"
-                id="inline-radio-2"
-                value={1}
-                checked={values.isRequired === 1}
-                onChange={() => setFieldValue('isRequired', 1)}
-              />
-              <Form.Check
-                label={t('wrong')}
-                name="isRequired"
-                type="radio"
-                id="inline-radio-3"
-                value={0}
-                checked={values.isRequired === 0}
-                onChange={() => setFieldValue('isRequired', 0)}
-              />
-            </Form.Group>
-            <Button className="submitButton" type="submit">{t('buttonSubmitNewDifferential')}</Button>
-          </Form>
-        </div>
-      )}
-    </Formik>
+    <FormikProvider value={formik}>
+      <Form noValidate onSubmit={formik.handleSubmit}>
+        <CustomTypeaheadSelect
+          name="name"
+          label={t('addOption')}
+          options={options}
+        />
+        <Form.Group controlId="description">
+          <Form.Label>{t('description')}</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            {...formik.getFieldProps('description')}
+          />
+        </Form.Group>
+        <Form.Group controlId="isRequired">
+          <Form.Check
+            label={t('required')}
+            name="isRequired"
+            type="radio"
+            value={2}
+            onChange={formik.getFieldProps('isRequired').onChange}
+          />
+          <Form.Check
+            label={t('voluntary')}
+            name="isRequired"
+            type="radio"
+            value={1}
+            onChange={formik.getFieldProps('isRequired').onChange}
+          />
+          <Form.Check
+            label={t('wrong')}
+            name="isRequired"
+            type="radio"
+            value={0}
+            onChange={formik.getFieldProps('isRequired').onChange}
+          />
+        </Form.Group>
+        <Button className="submitButton" type="submit">{t('buttonSubmitNewDifferential')}</Button>
+      </Form>
+    </FormikProvider>
   );
 };
 
--- taudinpurkauspeli/frontend/src/App/subprocedure/components/interviewSubProcedure/NewOptionGroupForm.js
+++ taudinpurkauspeli/frontend/src/App/subprocedure/components/interviewSubProcedure/NewOptionGroupForm.js
@@ -3,51 +3,46 @@ import { useTranslation } from 'react-i18next';
 import {
   Form, Button,
 } from 'react-bootstrap';
-import { Formik } from 'formik';
+import { useFormik, FormikProvider } from 'formik';
 import * as Yup from 'yup';
 import { useSelector } from 'react-redux';
 import CustomTypeaheadSelect from '../../../../utils/CustomTypeaheadSelect';
 
 const NewOptionGroupForm = ({ addOptionGroup }) => {
   const { t } = useTranslation();
-
-  const AddCheckSchema = Yup.object().shape({
-    name: Yup.string()
-      .min(2, t('warningShort'))
-      .max(999, t('warningLong'))
-      .required(t('warningRequired')),
-  });
-
   const optionGroups = useSelector((state) => state.optionGroups);
 
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+    },
+    validationSchema: Yup.object().shape({
+      name: Yup.string()
+        .min(2, t('warningShort'))
+        .max(999, t('warningLong'))
+        .required(t('warningRequired')),
+    }),
+    onSubmit: async (values) => {
+      const group = optionGroups.filter((r) => r.name === values.name);
+      addOptionGroup(
+        group.length === 0
+          ? values
+          : group[0],
+      );
+    },
+  });
+
   return (
-    <Formik
-      initialValues={{
-        name: '',
-      }}
-      validationSchema={AddCheckSchema}
-      onSubmit={async (values) => {
-        const group = optionGroups.filter((r) => r.name === values.name);
-        addOptionGroup(
-          group.length === 0
-            ? values
-            : group[0],
-        );
-      }}
-    >
-      {({ handleSubmit }) => (
-        <div>
-          <Form onSubmit={handleSubmit}>
-            <CustomTypeaheadSelect
-              name="name"
-              label={t('addOptionGroup')}
-              options={optionGroups}
-            />
-            <Button className="submitButton" type="submit">{t('buttonSubmitNewDifferential')}</Button>
-          </Form>
-        </div>
-      )}
-    </Formik>
+    <FormikProvider value={formik}>
+      <Form noValidate onSubmit={formik.handleSubmit}>
+        <CustomTypeaheadSelect
+          name="name"
+          label={t('addOptionGroup')}
+          options={optionGroups}
+        />
+        <Button className="submitButton" type="submit">{t('buttonSubmitNewDifferential')}</Button>
+      </Form>
+    </FormikProvider>
   );
 };
 
