cae16c81fef1e91adab4d3faac49b0dabc1b60af
Henna Mäkiluoma <henna.makiluoma@gmail.com>
2022-04-16 13:37:45 +0300

    standardized form naming

--- taudinpurkauspeli/frontend/src/tests/case/AddCaseForm.test.js
+++ taudinpurkauspeli/frontend/src/tests/case/AddCaseForm.test.js
@@ -3,7 +3,7 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import NewCase from '../../App/case/components/NewCaseForm';
+import AddCaseForm from '../../App/case/components/AddCaseForm';
 
 jest.mock('react-i18next', () => ({
   useTranslation: () => ({ t: (key) => key }),
@@ -12,7 +12,7 @@ jest.mock('react-i18next', () => ({
 test('<AddCase /> updates parent state and calls onSubmit', async () => {
   const addCase = jest.fn();
 
-  render(<NewCase addCase={addCase} />);
+  render(<AddCaseForm addCase={addCase} />);
 
   userEvent.type(screen.getByLabelText(/caseTitle/i), 'testTitle');
   userEvent.type(screen.getByLabelText(/caseAnamnesis/i), 'testAnamnesis');
@@ -29,7 +29,7 @@ test('<AddCase /> updates parent state and calls onSubmit', async () => {
 test('Case with invalid name cannot be created', async () => {
   const addCase = jest.fn();
 
-  render(<NewCase addCase={addCase} />);
+  render(<AddCaseForm addCase={addCase} />);
 
   userEvent.type(screen.getByLabelText(/caseTitle/i), 't');
   userEvent.type(screen.getByLabelText(/caseAnamnesis/i), 'testAnamnesis');
