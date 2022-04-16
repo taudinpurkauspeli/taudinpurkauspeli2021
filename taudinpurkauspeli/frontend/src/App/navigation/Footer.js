9ff95f973a6462362c77a10265792a90e7fc8614
Henna Mäkiluoma <henna.makiluoma@gmail.com>
2022-04-16 14:10:31 +0300

    some more betterments for footer

--- taudinpurkauspeli/frontend/src/App/navigation/Footer.js
+++ taudinpurkauspeli/frontend/src/App/navigation/Footer.js
@@ -5,10 +5,20 @@ import { useTranslation } from 'react-i18next';
 const Footer = () => {
   const { t } = useTranslation();
 
+  const phantom = {
+    display: 'block',
+    padding: '40px',
+    height: '60px',
+    width: '100%',
+  };
+
   return (
-    <div className="footer">
-      <hr />
-      <Link to="/privacy" style={{ color: 'black' }}>{t('privacyNotice')}</Link>
+    <div>
+      <div style={phantom} />
+      <div className="footer">
+        <hr />
+        <Link to="/privacy" style={{ color: 'black' }}>{t('privacyNotice')}</Link>
+      </div>
     </div>
   );
 };
