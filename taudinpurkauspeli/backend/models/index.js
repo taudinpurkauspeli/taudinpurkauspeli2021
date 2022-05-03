5855626476374d41b22249e4c9f50f7d6c87ab30
Dennis <tmi.dennisapter@gmail.com>
2022-05-03 13:55:52 +0300

    trying new ssl way take 3

--- taudinpurkauspeli/backend/models/index.js
+++ taudinpurkauspeli/backend/models/index.js
@@ -3,21 +3,7 @@
 const Sequelize = require('sequelize');
 const dbConfig = require('../utils/config');
 
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  operatorsAliases: 0,
-
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle,
-  },
-
-  logging: false,
-  ssl: true,
-});
+const sequelize = new Sequelize(dbConfig.HOST);
 
 const db = {};
 
